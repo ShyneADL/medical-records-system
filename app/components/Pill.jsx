30b0dc7922f0f511333b8d579a28bbe3a46f586c
ShyneADL <benex2002@gmail.com>
2024-02-20 17:18:02 +0100

    fixed the pill sliding

--- app/components/Pill.jsx
+++ app/components/Pill.jsx
@@ -4,7 +4,7 @@ const Pill = ({first, second, third}) => {
   return (
     <div class="container">
 	<div class="tabs">
-		<input type="radio" id="radio-1" name="tabs" checked />
+		<input type="radio" id="radio-1" name="tabs" />
 		<label class="tab" for="radio-1">{first}</label>
 		<input type="radio" id="radio-2" name="tabs" />
 		<label class="tab" for="radio-2">{second}</label>
